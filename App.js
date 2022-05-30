bf9ddabcbffe06170252289ed7fca8b0deadde26
maxdesanta <maxdesanta1@gmail.com>
2022-05-30 14:34:10 +0700

    latihan expo

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './Screens/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Hello world</Text>
+      </View>
+    </NavigationContainer>
   );
 }
 
@@ -16,5 +20,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
---
+++ Screens/Gallery.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function GalleryScreen() {
+  return (
+    <View>
+      <Text>GalleryScreen</Text>
+    </View>
+  )
+}
